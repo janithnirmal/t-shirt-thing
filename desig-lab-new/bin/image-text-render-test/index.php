39e6731bc68268286b5f3cf67033a8b483eafef0
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-08-07 23:16:36 +0530

    updated

---
+++ desig-lab-new/bin/image-text-render-test/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="../../bootstrap.css">
+    <link rel="stylesheet" href="imageTextControllerStyles.css">
+
+    <!-- js -->
+    <script src="https://cdn.jsdelivr.net/npm/interactjs/dist/interact.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.5.0/fabric.min.js"></script>
+    <script src="imageTextController.js"></script>
+</head>
+
+<body class="bg-dark d-flex justify-content-center align-items-center" style="height: 100vh;">
+
+    <div class="bg-danger" style="width: 400px; height: 540px;"></div>
+
+</body>
+
+</html>
