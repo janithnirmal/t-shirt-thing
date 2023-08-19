959d839aca8ea4eb2ac9a9315c341faef3164ac1
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-08-20 00:28:10 +0530

    updated

--- desig-lab-new/index.php
+++ desig-lab-new/index.php
@@ -1185,12 +1185,11 @@
         <div id="renderStartToastMessage" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <img src="..." class="rounded me-2" alt="...">
-                <strong class="me-auto">Bootstrap</strong>
-                <small>11 mins ago</small>
+                <strong class="me-auto">Saving Design</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                Hello, world! This is a toast message.
+                Wait for few seconds.....
             </div>
         </div>
     </div>
