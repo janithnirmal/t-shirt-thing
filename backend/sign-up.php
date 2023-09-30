5fd1e6809a81a8fdaf8bcdfd37f558f740274e33
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-09-30 15:06:42 +0530

    signup bug fixed

--- backend/sign-up.php
+++ backend/sign-up.php
@@ -3,6 +3,7 @@ require_once 'app/database_driver.php';
 require_once 'app/response_sender.php';
 require_once 'app/passwordEncryptor.php';
 require_once 'email_save.php';
+require_once 'app/mail/MailSender.php';
 
 // Initialize the response object
 $responseObject = new stdClass();
@@ -14,11 +15,11 @@ if (!isset($_POST['email']) || !isset($_POST['password'])) {
 
 // Save the email to the session
 $email = $_POST['email'];
-email_server::emailSave($email);
+// email_server::emailSave($email);
 
-// Retrieve the email from the session
-$sessionEmail = email_server::get_email();
-echo "Email retrieved from session: $sessionEmail";
+// // Retrieve the email from the session
+// $sessionEmail = email_server::get_email();
+// echo "Email retrieved from session: $sessionEmail";
 
 $decrypt_email = $_POST['email'];
 $decrypt_password = $_POST['password'];
@@ -26,9 +27,12 @@ $decrypt_password = $_POST['password'];
 $email = base64_encode($decrypt_email);
 $password = base64_encode($decrypt_password);
 
-$login_link =  $_SERVER['HTTP_HOST'] ."/backend/user_verification.php?email={$email}&password={$password}";
+$login_link =  $_SERVER['HTTP_HOST'] . "/backend/user_verification.php?email={$email}&password={$password}";
 
-echo "Login link: $login_link";
+
+$mailSender = new MailSender($email);
+$mailSender->mailInitiate("Sign UP Link - Design Lab", "Click on the following link to verify", $login_link);
+$mailSender->sendMail();
 
 // Send a response
 $responseObject->status = "success";
